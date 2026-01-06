98085f7b789b3cfea36ec5341467e5488ea51255
Hasnain <hasnainr0111@gmail.com>
2026-01-06 22:20:45 +0500

    f

--- components/home/FeaturedProducts.jsx
+++ components/home/FeaturedProducts.jsx
@@ -14,7 +14,7 @@ export default function FeaturedProducts() {
         <section className="section-padding bg-white">
             <Container>
                 <div className="text-center mb-12">
-                    <h2 className="text-4xl text-white md:text-5xl font-heading font-bold text-primary mb-4">
+                    <h2 className="text-4xl text-[#0b2f26] md:text-5xl font-heading font-bold text-primary mb-4">
                         Featured Collection
                     </h2>
                     <p className="text-lg text-neutral-gray max-w-2xl mx-auto">
